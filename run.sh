9901c993b7771b48a77fea170c6c438fcac11f02
Mehmet Aktas <mfatihaktas@gmail.com>
2021-06-25 16:08:04 +0300

    Sync run.sh

--- run.sh
+++ run.sh
@@ -18,8 +18,17 @@ elif [ $1 = 'cm' ]; then
   # $PY -u client.py --i=$2 --mid_addr_m='{"m0": ["192.168.49.2", "null"]}' --mport=30000
   # $PY -u client.py --i=$2 --mid_addr_m='{"m0": ["10.106.162.193", "null"]}' --mport=30000
   $PY -u client.py --i=$2 --mean_inter_gen_time=1 --num_reqs_to_finish=100000 --mid_addr_m='{"m0": ["127.0.0.1", '$3']}'
+elif [ $1 = 'cmo' ]; then
+  M0_IP=
+  DASHBOARD_SERVER_IP=
+  $PY -u client.py --i=$2 --mean_inter_gen_time=0.12 --num_reqs_to_finish=100000 --mid_addr_m='{"'$M0_IP'": ["10.101.185.226", "null"]}' --dashboard_server_addr='["'$DASHBOARD_SERVER_IP'", "null"]'
 elif [ $1 = 'cm2' ]; then
   $PY -u client.py --log_to_std=0 --i=$2 --d=2 --inter_probe_num_reqs=20 --mean_inter_gen_time=0.1 --num_reqs_to_finish=100000 --mid_addr_m='{"m0": ["127.0.0.1", '$3'], "m1": ["127.0.0.1", '$4']}' --dashboard_server_addr='["127.0.0.1", '$5']'
+elif [ $1 = 'cmo2' ]; then
+  M0_IP=
+  M1_IP=
+  DASHBOARD_SERVER_IP=
+  $PY -u client.py --i=$2 --d=2 --inter_probe_num_reqs=20 --mean_inter_gen_time=0.1 --num_reqs_to_finish=100000 --mid_addr_m='{"m0": ["'$M0_IP'", "null"], "m1": ["'$M1_IP'", "null"]}' --dashboard_server_addr='["'$DASHBOARD_SERVER_IP'", "null"]'
 elif [ $1 = 'm' ]; then
   # $PY -u master.py --i=$2 --wip_l='["10.0.1.0"]'
   $PY -u master.py --log_to_std=0 --i=$2 --wip_l='["10.0.1.0","10.0.1.1"]' --dashboard_server_ip='10.0.3.0'
