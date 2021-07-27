bb01cc4e27d1b225c5d5688259ecf0cdc762ccf2
Mehmet Aktas <mfatihaktas@gmail.com>
2021-07-27 17:50:43 +0300

    Moved common sim files into sim_common

--- sim_config.py
+++ sim_config.py
@@ -18,7 +18,7 @@ inter_req_gen_time_rv = Exp(req_gen_rate) # DiscreteRV(p_l=[1], v_l=[1 / req_gen
 serv_rate = 1
 serv_time_rv = Exp(serv_rate) # DiscreteRV(p_l=[1], v_l=[1 / serv_rate])
 
-fluctuating_net_state = True # False
+fluctuating_net = True # False
 net_delay = 0
 net_delay_additional = 5
 num_fluctuating_cl = 2
@@ -27,7 +27,7 @@ slow_dur_rv = DiscreteRV(p_l=[1], v_l=[100])
 
 def log_global_vars():
 	log(INFO, "", N=N, n=n, m=m, ro=ro, inter_req_gen_time_rv=inter_req_gen_time_rv, serv_time_rv=serv_time_rv,
-			fluctuating_net_state=fluctuating_net_state, net_delay=net_delay, net_delay_additional=net_delay_additional,
+			fluctuating_net=fluctuating_net, net_delay=net_delay, net_delay_additional=net_delay_additional,
 			num_fluctuating_cl=num_fluctuating_cl, normal_dur_rv=normal_dur_rv, slow_dur_rv=slow_dur_rv)
 
 def get_inter_req_gen_time_rv(m):
