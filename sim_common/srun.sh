3faecaa2a001117206574edfca0ba854702c1548
Mehmet Aktas <mfatihaktas@gmail.com>
2021-09-03 17:49:52 +0300

    Modified srun.sh slightly

--- sim_common/srun.sh
+++ sim_common/srun.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 echo $1 $2 $3
 
+COUNTER=1
+
+srun_ () {
+  [ -z "$1" ] && { echo "Subfolder?"; exit 1; }
+  SUBFOLDER="$1"
+  echo "SUBFOLDER=${SUBFOLDER}"
+  [ -z "$2" ] && { echo "hetero_clusters?"; exit 1; }
+  HETERO_CLUSTERS="$2"
+  echo "HETERO_CLUSTERS=${HETERO_CLUSTERS}"
+  [ -z "$3" ] && { echo "N_fluctuating_frac?"; exit 1; }
+  N_FLUCTUATING_FRAC="$3"
+  echo "N_FLUCTUATING_FRAC=${N_FLUCTUATING_FRAC}"
+  [ -z "$4" ] && { echo "serv_time_rv?"; exit 1; }
+  SERV_TIME_RV="$4"
+  echo "SERV_TIME_RV=${SERV_TIME_RV}"
+
+  ./srun.sh r $SUBFOLDER "--hetero_clusters=${HETERO_CLUSTERS} --N_fluctuating_frac=${N_FLUCTUATING_FRAC} --serv_time_rv=${SERV_TIME_RV}" $5
+  COUNTER=$((COUNTER + 1))
+}
+
+srun_w_label () {
+  [ -z "$1" ] && { echo "Label? podc, ts, rr, ucb?"; exit 1; }
+  SUBFOLDER="sim_$1"
+  rm sbatch_*
+  rm ../$SUBFOLDER/log/*
+
+  srun_ $SUBFOLDER 0 0   'disc' $COUNTER
+  srun_ $SUBFOLDER 0 0   'exp'  $COUNTER
+  srun_ $SUBFOLDER 0 0.3 'exp'  $COUNTER
+  srun_ $SUBFOLDER 0 0.3 'exp'  $COUNTER
+
+  srun_ $SUBFOLDER 1 0   'disc' $COUNTER
+  srun_ $SUBFOLDER 1 0   'exp'  $COUNTER
+  srun_ $SUBFOLDER 1 0.3 'disc' $COUNTER
+  srun_ $SUBFOLDER 1 0.3 'exp'  $COUNTER
+}
+
 if [ $1 = 'i' ]; then
   srun --partition=main --nodes=1 --ntasks=1 --cpus-per-task=1 --mem=4000 --time=3:00:00 --export=ALL --pty bash -i
 elif [ $1 = 'r' ]; then
@@ -35,43 +72,6 @@ srun --mpi=pmi2 python3 ${FILE_PATH} ${OPTS}
 
   sbatch $SCRIPT_NAME
 elif [ $1 = 'rc' ]; then
-  COUNTER=1
-
-  srun_ () {
-    [ -z "$1" ] && { echo "Subfolder?"; exit 1; }
-    SUBFOLDER="$1"
-    echo "SUBFOLDER=${SUBFOLDER}"
-    [ -z "$2" ] && { echo "hetero_clusters?"; exit 1; }
-    HETERO_CLUSTERS="$2"
-    echo "HETERO_CLUSTERS=${HETERO_CLUSTERS}"
-    [ -z "$3" ] && { echo "N_fluctuating_frac?"; exit 1; }
-    N_FLUCTUATING_FRAC="$3"
-    echo "N_FLUCTUATING_FRAC=${N_FLUCTUATING_FRAC}"
-    [ -z "$4" ] && { echo "serv_time_rv?"; exit 1; }
-    SERV_TIME_RV="$4"
-    echo "SERV_TIME_RV=${SERV_TIME_RV}"
-
-    ./srun.sh r $SUBFOLDER "--hetero_clusters=${HETERO_CLUSTERS} --N_fluctuating_frac=${N_FLUCTUATING_FRAC} --serv_time_rv=${SERV_TIME_RV}" $5
-    COUNTER=$((COUNTER + 1))
-  }
-
-  srun_w_label () {
-    [ -z "$1" ] && { echo "Label? podc, ts, rr, ucb?"; exit 1; }
-    SUBFOLDER="sim_$1"
-    rm sbatch_*
-    rm ../$SUBFOLDER/log/*
-
-    srun_ $SUBFOLDER 0 0   'disc' $COUNTER
-    srun_ $SUBFOLDER 0 0   'exp'  $COUNTER
-    srun_ $SUBFOLDER 0 0.3 'exp'  $COUNTER
-    srun_ $SUBFOLDER 0 0.3 'exp'  $COUNTER
-
-    srun_ $SUBFOLDER 1 0   'disc' $COUNTER
-    srun_ $SUBFOLDER 1 0   'exp'  $COUNTER
-    srun_ $SUBFOLDER 1 0.3 'disc' $COUNTER
-    srun_ $SUBFOLDER 1 0.3 'exp'  $COUNTER
-  }
-
   srun_w_label 'podc'
   srun_w_label 'ts'
   srun_w_label 'rr'
